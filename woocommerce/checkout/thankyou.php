93c18e51b18cbbda09f2d312703b0457aa947c30
bintangmogot <harunbintangmogot07@gmail.com>
2026-06-30 02:29:07 +0800

    style: add flex-wrap to thank you page logos

--- woocommerce/checkout/thankyou.php
+++ woocommerce/checkout/thankyou.php
@@ -260,7 +260,7 @@ defined( 'ABSPATH' ) || exit;
             </div>
 
             <!-- Payment Methods Images -->
-            <div class="flex justify-center items-center gap-4 lg:gap-8 pb-10">
+            <div class="flex flex-wrap justify-center items-center gap-4 lg:gap-8 pb-10">
                 <img src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/commonwealth.png' ); ?>" alt="Commonwealth Bank" class="!h-[12px] sm:!h-[30px] object-contain">
                 <img src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/pay-idi.png' ); ?>" alt="PayID" class="!h-[12px] sm:!h-[30px] object-contain">
                 <img src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/osko-1.jpg' ); ?>" alt="Osko by BPAY" class="!h-[12px] sm:!h-[30px] object-contain">
